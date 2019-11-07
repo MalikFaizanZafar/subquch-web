ac45bb46488d5724d9c932e9d3a25e203ca0d9f6
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-11-07 13:04:35 +0500

    functionality to add project to db

--- src/app/modules/admin/pages/new-project-page/new-project-page.component.ts
+++ src/app/modules/admin/pages/new-project-page/new-project-page.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { BuisnessModel } from './../../models/buisness.model'
+import { BuisnessService } from '../../services/buisness.service';
+import { IsButton } from 'app/lib';
+import { Router } from '@angular/router';
 @Component({
   selector: 'new-project-page',
   templateUrl: './new-project-page.component.html',
   styleUrls: ['./new-project-page.component.css']
 })
 export class NewProjectPageComponent implements OnInit {
-
-  constructor() { }
+  buisnessForm: FormGroup;
+  constructor(private buisnessService: BuisnessService, private router : Router) { }
 
   ngOnInit() {
+    this.buisnessForm = new FormGroup({
+      title: new FormControl(null, [Validators.required]),
+      totalWorth: new FormControl(null, [Validators.required]),
+      monthlyWorth: new FormControl(null, [Validators.required]),
+      startDate: new FormControl(null, [Validators.required]),
+      endDate: new FormControl(null, [Validators.required]),
+      disclaimer: new FormControl(null, [Validators.required]),
+      summary: new FormControl(null, [Validators.required]),
+      detailDescription: new FormControl(null, [Validators.required])
+    });
   }
 
+  onBuisnessFormSubmit(btn : IsButton){
+    btn.startLoading();
+    let formValues = this.buisnessForm.value;
+    let newBuisness: BuisnessModel = formValues;
+    newBuisness.remainingShares = 0;
+    newBuisness.logoUrl = "";
+    newBuisness.enabled = false;
+    newBuisness.images = [];
+    console.log("newBuisness ", newBuisness)
+    this.buisnessService.addBuisness(newBuisness).subscribe(res => {
+      console.log("res is : ", res);
+      btn.stopLoading();
+      this.router.navigate(['admin', 'projects'])
+    }, err => {
+      btn.stopLoading();
+    })
+  }
 }
--- src/app/modules/admin/services/buisness.service.ts
+++ src/app/modules/admin/services/buisness.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment } from "environments/environment";
+import { BuisnessModel } from "../models/buisness.model";
 
 
 const baseUrl = environment.baseUrl;
@@ -16,6 +17,15 @@ export class BuisnessService {
     const token = localStorage.getItem("Authorization");
     headers = headers.append("Authorization", token);
     return this.http.get<any>(`${this.baseURL}/buisness`, {
+      headers
+    });
+  }
+
+  addBuisness(buisness : BuisnessModel): Observable<any>{
+    let headers = new HttpHeaders();
+    const token = localStorage.getItem("Authorization");
+    headers = headers.append("Authorization", token);
+    return this.http.post<any>(`${this.baseURL}/buisness`, buisness, {
       headers
     });
   }
